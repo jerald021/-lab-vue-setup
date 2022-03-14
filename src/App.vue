50976285a7596a1b92b9e19967587c8d209ec673
Jerald Osco <jerald021@gmail.com>
2022-03-14 17:01:27 +0100

    done

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{text1}}</h1>
+    <button @click="helloWorld">Click me </button>
+
+    <p>
+      <router-link to="/">Home</router-link>
+      <br>
+      <router-link to="/about">About</router-link>
+    </p>
+    <router-view></router-view>
+
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Footer from './components/Footer.vue';
+export default {
+  data(){
+    return{
+      text1: 'ejemplo de texto',      
+    }
+  },
+  components:{
+    Footer,
+  },
+  methods:{
+    helloWorld(){
+      console.log('Hello world');
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>esto es el footer Copyright</p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
